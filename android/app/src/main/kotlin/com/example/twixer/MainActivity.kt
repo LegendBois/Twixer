93d084f0f87d83aecf2b2d15259cf80d3dc3e740
AnmolMajithia <rush2asm@gmail.com>
2020-07-11 19:29:06 +0530

    ME IZ AMAZE

---
+++ android/app/src/main/kotlin/com/example/twixer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.twixer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login/login.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+
+import 'package:twixer/home/home.dart';
+
+const users = const {
+  'majithiaanmol12@gmail.com': '123456',
+  'test@gmail.com': 'legendbois',
+};
+
+class Login extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String> _authUser(LoginData data) {
+    print('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'Username not exists';
+      }
+      if (users[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'Username not exists';
+      }
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      theme: LoginTheme(
+        bodyStyle: TextStyle(
+          color: Colors.white,
+        ),
+        primaryColor: Color.fromARGB(255, 194, 50, 100),
+        accentColor: Colors.white,
+        inputTheme: InputDecorationTheme(
+          fillColor: Colors.white,          
+        ),
+      ),
+      title: 'TWIXER',
+      logo: '../../../assets/images/twixer-logo.png', //get this to work
+      onLogin: _authUser,
+      onSignup: _authUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => Home(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
